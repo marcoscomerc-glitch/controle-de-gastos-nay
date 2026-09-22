281ab211c620546f67b2adf5321bddb47fc0ee8e
Marocos Antonio <marcos.comerc@gmail.com>
2026-09-22 00:51:42 -0300

    Add files via upload
    Avatar, reservas e ícones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'controle-gastos-v4';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'controle-gastos-v5';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(

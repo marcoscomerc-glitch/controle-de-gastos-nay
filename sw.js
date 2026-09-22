a74d76bbd32d365acb9d50b14a6fd6190daa9424
Marocos Antonio <marcos.comerc@gmail.com>
2026-09-22 10:16:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'controle-gastos-v7';
+const CACHE = 'controle-gastos-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
